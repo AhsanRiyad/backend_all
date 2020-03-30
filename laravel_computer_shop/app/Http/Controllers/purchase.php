990bddacef609aa25cf68c5a7b3f2f745cbceb0b
unknown <riyad298@gmail.com>
2020-03-30 16:23:22 +0600

    commit by alias
    this is used for short cut puspose

--- laravel_computer_shop/app/Http/Controllers/purchase.php
+++ laravel_computer_shop/app/Http/Controllers/purchase.php
@@ -264,100 +264,31 @@ class purchase extends Controller
 	//add_purchase
 	function add_purchase(Request $req){
 
-
-		//invoice nubmer generation
-		
-
-		$invoice_number = 
+		//step_1
+		//insert to purchase_or_sell
 		DB::table('purchase_or_sell')
-		->max('invoice_number');
+		->insert( $req->purchase_or_sell );
 
+		//step_2
+		//insert to sell_or_purchase_details
+		foreach ($req->sell_or_purchase_details as $key => $value) {
 
-
-		// echo 'invoice_number';
-		// echo $invoice_number;
-		if($invoice_number == ''){
-
-			$invoice_number = 100000;
-
-		}else{
-
-			$invoice_number += 1; 
-		}
-		
-
-
-		//adding serial
-		$i = 0;
-		for($i = 0 ; $i < count($req->purchase_data['serial']); $i++ ){
-
-			echo 'serial_number';
-			echo $req->purchase_data['serial'][$i]['serial_number'];
-
-			DB::table('serial_number')->insert(
-				[
-					'invoice_number' => $invoice_number, 
-					'product_id' => $req->purchase_data['serial'][$i]['p_id'],
-					'serial_number' => $req->purchase_data['serial'][$i]['serial_number'],
-					'status' => $req->purchase_data['serial'][$i]['status']
-				]
-			);
-
-
+			DB::table('sell_or_purchase_details')
+            ->insert(
+                (array) $value
+            );
 		}
 
-		// $affected = DB::table('serial_number')
-		// ->where('invoice_number', $invoice_number)
-		// ->update(['status' => 'Purchase']);
+		//step_3
+		//insert to serial_number
+		foreach ($req->serial_number as $key => $value) {
 
-
-
-		DB::table('purchase_or_sell')->insert(
-			[
-				'invoice_number' => $invoice_number,
-				'date' => $req->purchase_data['date'] ,
-				'reference_number' => $req->purchase_data['reference_number'],
-				'warehouse_id' => $req->purchase_data['warehouse_id'],
-				'supplier_id' => $req->purchase_data['supplier_id'],
-				'status' => $req->purchase_data['status'],
-				'type' => 'purchase' ,
-				'correction_status' => $req->purchase_data['correction_status'] ,
-			]
-
-		);
-
-		$i = 0;
-		for($i = 0 ; $i < count($req->purchase_data['products']); $i++ ){
-
-			//
-			DB::table('sell_or_purchase_details')->insert(
-				[
-					'invoice_number' => $invoice_number, 
-					'product_id' => $req->purchase_data['products'][$i]['p_id'],
-					'quantity' => $req->purchase_data['products'][$i]['selling_quantity'],
-					'unit_price' => $req->purchase_data['products'][$i]['purchase_cost'],
-					'status' => $req->purchase_data['products'][$i]['status'],
-				]
-			);
-			//
-
-			// echo $req->purchase_data['products'][$i]['status'];
-
-
+			DB::table('serial_number')
+            ->insert(
+                (array) $value
+            );
 		}
 
-
-		$affected = DB::table('purchase_or_sell')
-		->where('invoice_number', $invoice_number)
-		->update(['insert_time_date' => DB::raw('sysdate()')]);
-
-
-
-		// return $req->purchase_data['date'];
-		return $req;
-		// return $req->purchase_data['products'][0];
-		// return $req->purchase_data['products'][0]['product_name'];
-		// return $invoice_number;
 	}
 
 
